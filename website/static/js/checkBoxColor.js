9e9f6a538efe663c769734b5b5a27cceb34d8103
CreoMaster <sunnatullazayniddinov@gmail.com>
2023-08-12 22:19:43 +0500

    small changes

--- website/static/js/checkBoxColor.js
+++ website/static/js/checkBoxColor.js
@@ -1,6 +1,6 @@
 var checkboxes = document.querySelectorAll('.item-checkbox');
     for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].value == 'V') {
+        if (checkboxes[i].value == 'O') {
             checkboxes[i].classList.add('completed');
         } else {
             checkboxes[i].classList.add('not-completed');
